8cee4ce49e1f73f57152f7953f95d82f9ee27f08
jiseonmin <minjiseon0827@gmail.com>
2025-12-05 11:10:47 -0500

    break up lines for readability, move path to slim code location

--- scripts/02_run_simulations/example_job_array.sh
+++ scripts/02_run_simulations/example_job_array.sh
@@ -20,12 +20,44 @@ LINE_NUM=$((SLURM_ARRAY_TASK_ID + 1))
 PARAMS=$(sed -n "${LINE_NUM}p" "$CSV_FILE")
 
 # Parse CSV - add more variables as needed
-IFS=',' read -r seed RUNTIME BURNIN LOGINTERVAL N_POP RECOVERY GEN_LEN_DEPENDS_ON_TEMP FIXED_GEN_LEN USE_EXTERNAL_TEMP_DATA TEMPDATA_PATH MEAN_TEMP STDEV_TEMP NUM_REP_TEMP_DATA B_default CTmin_default B_critical DeltaB CTmin_critical DeltaCTmin CTmax_critical DeltaCTmax OUTDIR OUTNAME  <<< "$PARAMS"
+IFS=',' read -r seed RUNTIME BURNIN\
+ LOGINTERVAL N_POP RECOVERY GEN_LEN_DEPENDS_ON_TEMP\
+ FIXED_GEN_LEN USE_EXTERNAL_TEMP_DATA TEMPDATA_PATH\
+  MEAN_TEMP STDEV_TEMP NUM_REP_TEMP_DATA B_default\
+   CTmin_default B_critical DeltaB CTmin_critical\
+    DeltaCTmin CTmax_critical DeltaCTmax\
+     OUTDIR OUTNAME  <<< "$PARAMS"
 
-# set the absolute path of where the output should go
-echo "Running job ${SLURM_ARRAY_TASK_ID} with seed=${seed} RUNTIME=${RUNTIME} BURNIN=${BURNIN} LOGINTERVAL=${LOGINTERVAL} N_POP=${N_POP} RECOVERY=${RECOVERY} GEN_LEN_DEPENDS_ON_TEMP=${GEN_LEN_DEPENDS_ON_TEMP} FIXED_GEN_LEN=${FIXED_GEN_LEN} USE_EXTERNAL_TEMP_DATA=${USE_EXTERNAL_TEMP_DATA} TEMPDATA_PATH=${TEMPDATA_PATH} MEAN_TEMP=${MEAN_TEMP} STDEV_TEMP=${STDEV_TEMP} NUM_REP_TEMP_DATA=${NUM_REP_TEMP_DATA} B_default=${B_default} CTmin_default=${CTmin_default} B_critical=${B_critical} DeltaB=${DeltaB} CTmin_critical=${CTmin_critical} DeltaCTmin=${DeltaCTmin} CTmax_critical=${CTmax_critical} DeltaCTmax=${DeltaCTmax} OUTDIR=${OUTDIR} OUTNAME=${OUTNAME}"
+echo "Running job ${SLURM_ARRAY_TASK_ID} \
+with seed=${seed} RUNTIME=${RUNTIME} BURNIN=${BURNIN} \
+LOGINTERVAL=${LOGINTERVAL} N_POP=${N_POP} RECOVERY=${RECOVERY}\
+ GEN_LEN_DEPENDS_ON_TEMP=${GEN_LEN_DEPENDS_ON_TEMP} \
+ FIXED_GEN_LEN=${FIXED_GEN_LEN} USE_EXTERNAL_TEMP_DATA=${USE_EXTERNAL_TEMP_DATA}\
+  TEMPDATA_PATH=${TEMPDATA_PATH} MEAN_TEMP=${MEAN_TEMP}\
+   STDEV_TEMP=${STDEV_TEMP} NUM_REP_TEMP_DATA=${NUM_REP_TEMP_DATA}\
+    B_default=${B_default} CTmin_default=${CTmin_default}\
+     B_critical=${B_critical} DeltaB=${DeltaB} \
+     CTmin_critical=${CTmin_critical} DeltaCTmin=${DeltaCTmin} CTmax_critical=${CTmax_critical} \
+     DeltaCTmax=${DeltaCTmax} OUTDIR=${OUTDIR} OUTNAME=${OUTNAME}"
+
+# Move to slim path
+SLIM_PATH="/home/j.min/TPC_evolution_SLiM/slim"
+cd "$SLIM_PATH"
 
 # Run slim script (check the absolute path of the slim script)
-slim -d seed=${seed} -d RUNTIME=${RUNTIME} -d BURNIN=${BURNIN} -d LOGINTERVAL=${LOGINTERVAL} -d N_POP=${N_POP} -d RECOVERY=${RECOVERY} -d GEN_LEN_DEPENDS_ON_TEMP=${GEN_LEN_DEPENDS_ON_TEMP} -d FIXED_GEN_LEN=${FIXED_GEN_LEN} -d USE_EXTERNAL_TEMP_DATA=${USE_EXTERNAL_TEMP_DATA} -d TEMPDATA_PATH=\'${TEMPDATA_PATH}\' -d MEAN_TEMP=${MEAN_TEMP} -d STDEV_TEMP=${STDEV_TEMP} -d NUM_REP_TEMP_DATA=${NUM_REP_TEMP_DATA} -d B_default=${B_default} -d CTmin_default=${CTmin_default} -d B_critical=${B_critical} -d DeltaB=${DeltaB} -d CTmin_critical=${CTmin_critical} -d DeltaCTmin=${DeltaCTmin} -d CTmax_critical=${CTmax_critical} -d DeltaCTmax=${DeltaCTmax} -d OUTDIR=\'${OUTDIR}\' -d OUTNAME=\'${OUTNAME}\' /home/j.min/TPC_evolution_SLiM/slim/master_WF.slim
+slim -d seed=${seed} -d RUNTIME=${RUNTIME} -d BURNIN=${BURNIN} \
+-d LOGINTERVAL=${LOGINTERVAL} -d N_POP=${N_POP} -d RECOVERY=${RECOVERY} \
+-d GEN_LEN_DEPENDS_ON_TEMP=${GEN_LEN_DEPENDS_ON_TEMP} \
+-d FIXED_GEN_LEN=${FIXED_GEN_LEN} \
+-d USE_EXTERNAL_TEMP_DATA=${USE_EXTERNAL_TEMP_DATA}\
+ -d TEMPDATA_PATH=\'${TEMPDATA_PATH}\' \
+ -d MEAN_TEMP=${MEAN_TEMP} -d STDEV_TEMP=${STDEV_TEMP} \
+ -d NUM_REP_TEMP_DATA=${NUM_REP_TEMP_DATA} \
+ -d B_default=${B_default} -d CTmin_default=${CTmin_default} \
+ -d B_critical=${B_critical} -d DeltaB=${DeltaB} \
+ -d CTmin_critical=${CTmin_critical} -d DeltaCTmin=${DeltaCTmin}\
+  -d CTmax_critical=${CTmax_critical} -d DeltaCTmax=${DeltaCTmax}\
+   -d OUTDIR=\'${OUTDIR}\' -d OUTNAME=\'${OUTNAME}\' \
+   master_WF.slim
 
 echo "slim simulation finished. output name = ${OUTNAME}"
