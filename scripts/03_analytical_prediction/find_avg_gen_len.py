7d3afb5d1758d8b64c8849bfbf45db72f34f6d3d
jiseonmin <minjiseon0827@gmail.com>
2025-12-06 15:40:10 -0500

    script to calculate average generation length

---
+++ scripts/03_analytical_prediction/find_avg_gen_len.py
@@ -0,0 +1,24 @@
+import glob
+import pandas as pd
+import sys
+
+# Find a file that matches OUTPATH and OUTNAME pattern
+def find_avg_gen_len(OUTDIR, OUTNAME):
+    logs = glob.glob(f"{OUTDIR}/{OUTNAME}_seed_*.txt")
+
+    # Read first log found
+    log = pd.read_csv(logs[0])
+
+    # Total days between start to the end of log files divided by the number of gens = avg generation length
+    # Round it to the closest integer
+    days = list(log['day'])
+    gens = list(log['cycle'])
+    avg_gen_len = round((days[-1] - days[0]) / (gens[-1] - gens[0]))
+
+    return avg_gen_len
+
+if __name__=="__main__":
+    OUTDIR = sys.argv[1]
+    OUTNAME = sys.argv[2]
+   
+    print(find_avg_gen_len(OUTDIR, OUTNAME))
