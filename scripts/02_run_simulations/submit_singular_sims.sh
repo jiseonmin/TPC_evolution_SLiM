220a76a3ce57d0e283151dbb545b700338677193
jiseonmin <minjiseon0827@gmail.com>
2026-02-06 13:12:30 -0500

    submit two sims not using master script as a batch job

---
+++ scripts/02_run_simulations/submit_singular_sims.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=TPC_evol_singular
+#SBATCH --output=/scratch/j.min/slurm_out/job_%A.out
+#SBATCH --error=/scratch/j.min/slurm_err/job_%A.err
+#SBATCH --time=2-00:00:00
+#SBATCH --mem=4G
+#SBATCH --partition=lotterhos
+#SBATCH --cpus-per-task=1
+
+# submit singular slim simulations using default parameters
+
+module load anaconda3
+eval "$(conda shell.bash hook)"
+conda activate tpc_evo_slim
+
+# Move to slim path
+SLIM_PATH="/home/j.min/TPC_evolution_SLiM/slim"
+cd "$SLIM_PATH"
+slim variable_N.slim
+echo -e "seasonally variable N simulation done. Next sim,"
+slim scramble_temp_data.slim
+echo -e "scrambled VT sim done."
