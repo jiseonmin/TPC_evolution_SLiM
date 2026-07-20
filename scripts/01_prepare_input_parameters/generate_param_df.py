a6bda4bf5ccd0bdbdbae27b2209e4f58d50ab5a5
jiseonmin <minjiseon0827@gmail.com>
2026-07-20 10:06:48 -0400

    increase qtn var even more

--- scripts/01_prepare_input_parameters/generate_param_df.py
+++ scripts/01_prepare_input_parameters/generate_param_df.py
@@ -769,7 +769,7 @@ def sine4():
     param_unique_filename = 'sine4_params_unique.csv'
 
     # List of params to scan
-    MU_AND_QTN_VAR = [(1e-7, 0.05), (1e-8, 25), (1e-9, 800), (1e-6, 0.005), (1e-5, 0.0005)]
+    MU_AND_QTN_VAR = [(1e-7, 0.05), (1e-8, 25), (1e-9, 4000), (1e-6, 0.005), (1e-5, 0.0005)]
     # OUTNAME will reflect the change of these parameters
 
     # List of parameters to change from default values, but keep constant across all simulations
